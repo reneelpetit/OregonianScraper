fe559edf21742d40b258ce9c3cce6cfe432ae4ef
Your Name <renee.petit@gmail.com>
2019-11-23 22:55:43 -0800

    articles print to screen

--- public/app.js
+++ public/app.js
@@ -2,10 +2,31 @@ $("#scrapeArticles").on("click", function (event) {
     event.preventDefault();
     console.log("scrape articles button click triggered");
 
+    function printData(data) {
+      $("#articles").append(
+        "<div class='card-header'>" +
+        "<a class='btn btn-primary text-white' href='/save'>" +
+        "<i class='far fa-newspaper'></i>" + 
+        "Save Article</a>" +
+        "<a class='btn btn-primary text-white' href=" + data.articleLink + ">" +
+        "<i class='far fa-newspaper'></i>" + 
+        "Read Article</a>" +
+        "</div>" +
+
+        "<div class='card-body'>" +
+        "<blockquote class='blockquote mb-0'>" +
+            "<p>Title: " + data.articleTitle + "</p>" +
+            "<footer class='blockquote-footer'>" + 
+            "Author Name <cite title='Source Title'>" +
+            data.articleDate + "</cite></footer>" +
+        "</blockquote>" +
+        "</div>"
+      )
+    }
     $.getJSON("/articles", function(data) {
       console.log("/articles data is ", data);
         for (var i = 0; i < data.length; i++) {
-          console.log(data[i]);
+          printData(data[i]);
         }
       });
 });
